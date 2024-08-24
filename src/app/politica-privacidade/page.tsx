293920192ceb0948c7771dcd7fc238498e748b0b
Pablo Neres <pabloneres@hotmail.com>
2024-08-24 12:30:43 -0300

    first commit

--- src/app/politica-privacidade/page.tsx
+++ src/app/politica-privacidade/page.tsx
@@ -15,7 +15,7 @@ export default function PrivacyPolice() {
             </p>
             <p>O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle
                 sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas <a
-                    href='https://privacidade.me/' target='_BLANK' style={{ color: "#616060", textDecoration: "none" }}>políticas de
+                    href="https://privacidade.me/" target="_BLANK" style={{ color: "#616060", textDecoration: "none" }}>políticas de
                     privacidade</a>. </p>
             <p>Você é livre para recusar a nossa solicitação de informações pessoais, entendendo que talvez não possamos fornecer
                 alguns dos serviços desejados.</p>
@@ -23,17 +23,17 @@ export default function PrivacyPolice() {
                 informações pessoais. Se você tiver alguma dúvida sobre como lidamos com dados do usuário e informações pessoais,
                 entre em contato conosco.</p>
             <h2>Segurança do site Maion Tax</h2>
-            <p>O site é confiável e seguro para o usuário conforme informado pelo <a target='_BLANK' style={{
+            <p>O site é confiável e seguro para o usuário conforme informado pelo <a target="_BLANK" style={{
                 color: "#616060",
                 textDecoration: "none"
-            }} href='https://sitecheck.com.br' rel='noopener noreferrer'>Site
+            }} href="https://sitecheck.com.br" rel="noopener noreferrer">Site
                 Check</a>. A página verifica informações do site para identificar possíveis problemas de segurança. </p>
             <h2>Política de Cookies Maion Tax</h2>
             <h3>O que são cookies?</h3>
             <p>Como é prática comum em quase todos os sites profissionais, este site usa cookies, que são pequenos arquivos baixados
                 no seu computador, para melhorar sua experiência. Esta página descreve quais informações eles coletam, como as
                 usamos e por que às vezes precisamos armazenar esses cookies. Também compartilharemos como você pode impedir que
-                esses cookies sejam armazenados, no entanto, isso pode fazer o downgrade ou 'quebrar' certos elementos da
+                esses cookies sejam armazenados, no entanto, isso pode fazer o downgrade ou "quebrar" certos elementos da
                 funcionalidade do site.</p>
             <h3>Como usamos os cookies?</h3>
             <p>Utilizamos cookies por vários motivos, detalhados abaixo. Infelizmente, na maioria dos casos, não existem opções
@@ -77,7 +77,7 @@ export default function PrivacyPolice() {
             <p>Em alguns casos especiais, também usamos cookies fornecidos por terceiros confiáveis. A seção a seguir detalha quais
                 cookies de terceiros você pode encontrar através deste site.</p>
             <ul>
-                <li> Este site usa o <a href='https://analytics.google.com/analytics/web/'>Google Analytics</a>, que é uma das
+                <li> Este site usa o <a href="https://analytics.google.com/analytics/web/">Google Analytics</a>, que é uma das
                     soluções de análise mais difundidas e confiáveis ​​da Web, para nos ajudar a entender como você usa o site e
                     como podemos melhorar sua experiência. Esses cookies podem rastrear itens como quanto tempo você gasta no site e
                     as páginas visitadas, para que possamos continuar produzindo conteúdo atraente. </li>
@@ -112,17 +112,17 @@ export default function PrivacyPolice() {
                 realizar esse procedimento, acesse as configurações do seu browser. Consulte abaixo guias de ajuda dos principais
                 navegadores: </p>
             <ul>
-                <li> <a target='_blank' rel='noopener noreferrer' href='https://support.google.com/accounts/answer/61416?hl=en'>
+                <li> <a target="_blank" rel="noopener noreferrer" href="https://support.google.com/accounts/answer/61416?hl=en">
                     Google Chrome </a> </li>
-                <li> <a target='_blank' rel='noopener noreferrer'
-                    href='https://support.mozilla.org/en-US/kb/enhanced-tracking-protection-firefox-desktop?redirectlocale=en-US'>
+                <li> <a target="_blank" rel="noopener noreferrer"
+                    href="https://support.mozilla.org/en-US/kb/enhanced-tracking-protection-firefox-desktop?redirectlocale=en-US">
                     Firefox </a> </li>
-                <li> <a target='_blank' rel='noopener noreferrer'
-                    href='https://support.microsoft.com/pt-br/help/17442/windows-internet-explorer-delete-manage-cookies'>
+                <li> <a target="_blank" rel="noopener noreferrer"
+                    href="https://support.microsoft.com/pt-br/help/17442/windows-internet-explorer-delete-manage-cookies">
                     Microsoft Edge </a> </li>
-                <li> <a target='_blank' rel='noopener noreferrer'
-                    href='https://blogs.opera.com/news/2015/08/how-to-manage-cookies-in-opera/'> Opera </a> </li>
-                <li> <a target='_blank' rel='noopener noreferrer' href='https://support.apple.com/pt-br/guide/safari/sfri11471/mac'>
+                <li> <a target="_blank" rel="noopener noreferrer"
+                    href="https://blogs.opera.com/news/2015/08/how-to-manage-cookies-in-opera/"> Opera </a> </li>
+                <li> <a target="_blank" rel="noopener noreferrer" href="https://support.apple.com/pt-br/guide/safari/sfri11471/mac">
                     Safari </a>. </li>
             </ul>
             <h3>Mais informações</h3>

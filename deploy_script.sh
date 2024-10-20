622a30cb22f805362cd6349bdeeef19f0ba9fe97
Pablo Neres <pabloneres@hotmail.com>
2024-10-20 19:39:19 -0300

    ajustes imagens

--- deploy_script.sh
+++ deploy_script.sh
@@ -13,11 +13,11 @@ git pull || error_exit "Falha ao executar git pull"
 
 # Parar e iniciar os serviços Docker
 echo "Parando serviços Docker..."
-docker compose -f docker-compose.prod.yml down || error_exit "Falha ao parar serviços Docker"
+docker compose -f docker-compose.yml down || error_exit "Falha ao parar serviços Docker"
 
 docker system prune --all --force --volumes
 
 echo "Iniciando serviços Docker..."
-docker compose -f docker-compose.prod.yml up -d || error_exit "Falha ao iniciar serviços Docker"
+docker compose -f docker-compose.yml up -d || error_exit "Falha ao iniciar serviços Docker"
 
 echo "Deploy concluído com sucesso!"

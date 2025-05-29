14284d02a2a4c459fdb2ae407c44e21582d63d13
Pablo Neres <pabloneres@hotmail.com>
2025-05-29 10:38:48 -0300

    ajuste

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { FacebookPixelEvents } from "../components/pixel-events";
 import Script from "next/script";
 import Image from "next/image";
 import Link from "next/link";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 
 const poppins = Poppins({
@@ -48,8 +49,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W5XZX7Z4"
-        height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
+      <GoogleAnalytics gaId="GTM-TT3HWN64" />
       <body className={`${montsserat.variable} ${poppins.variable} ${cardo.variable}`}>
         {children}
         <div className="whatsapp">
@@ -61,13 +61,6 @@ export default function RootLayout({
       <Suspense fallback={null}>
         <FacebookPixelEvents />
       </Suspense>
-      <Script id="gtm" strategy="afterInteractive">
-        {`<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-W5XZX7Z4');</script>`}
-      </Script>
     </html>
   );
 }
